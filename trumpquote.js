a96a70ce3227e4304b1bc6e98b8d97a6f11b0c5c
Grace Barata <gracebarata@Graces-Air.lan>
2020-02-23 00:22:05 +0000

    styling-improved

--- trumpquote.js
+++ trumpquote.js
@@ -1,14 +1,32 @@
 function getANewJoke() {
-  fetch("https://www.tronalddump.io/random/quote", {})
-    .then(function(next) {
+  fetch("https://www.tronalddump.io/random/quote")
+    .then(function (next) {
       console.log(next);
       return next.json();
     })
-    .then(function(data) {
+    .then(function (data) {
       console.log(data);
-      document.getElementById("par").innerHTML = `<h1>${data.value}</h1>`;
+      document.getElementById("text").innerHTML = `<h1>${data.value}</h1>`;
     });
+
+}
+
+function rotatePic() {
+  let img = document.getElementById("trump-pic")
+  img.className = "rotate-center"
+
+  setTimeout(function () {
+    img.classList.remove("rotate-center")
+  }, 2000);
+
+
 }
 
 let jokeBtn = document.getElementById("getJoke");
-jokeBtn.addEventListener("click", getANewJoke());
+jokeBtn.addEventListener("click", getANewJoke);
+
+jokeBtn.addEventListener("click", rotatePic)
+
+
+
+
